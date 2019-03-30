a7406c0ab58372e8ade68df86fc36faaf1b9273e
DocPeteMac <42282203+DocPeteMac@users.noreply.github.com>
2019-03-30 00:14:04 -0500

    first upload Mar30

---
+++ js/index.js
@@ -0,0 +1,15 @@
+// take pattern number from input
+let go = document.querySelector('#goBtn');
+go.addEventListener('click', patternNumber);
+
+
+
+function patternNumber(){
+    let pNum = document.querySelector('#pattrnNum').value;
+    if (pNum > 10 || pNum < 1){
+        alert("The pattern #" + pNum + " is not available");
+    }else{
+        let img = document.querySelector('#pic');
+        img.src = "img/3D/0" + pNum + ".gif";
+    }
+}
